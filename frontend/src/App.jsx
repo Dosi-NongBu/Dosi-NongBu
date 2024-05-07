88cecb1df7b213c30412b6cb5386bb540e02186c
heemin728 <idkhm0728@naver.com>
2024-05-07 22:22:54 +0900

    feat : 화면 구조

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,64 @@
 import "./App.css";
-import React, {useEffect, useState} from 'react';
-import axios from 'axios';
+import { Routes, Route, Outlet } from "react-router-dom";
+
+import Navbar from "./layout/Navbar/Navbar";
+import Footer from "./layout/Footer/Footer";
+
+// import LandingPage from "./pages/LandingPage/LandingPage";
+// import LoginPage from "./pages/LandingPage/LoginPage";
+// import SearchCropPage from "./pages/NewCropPage/SearchCropPage";
+// import CropDetailPage from "./pages/NewCropPage/CropDetailPage";
+// import MyPage from "./pages/MyPage/MyPage";
+// import MyCropPage from "./pages/MyCropPage/MyCropPage";
+// import MyCropDetailPage from "./pages/MyCropPage/MyCropDetailPage";
+// import CommunityPage from "./pages/CommunityPage/CommunityPage";
+// import QuestionCommunityPage from "./pages/CommunityPage/QuestionCommunityPage";
+// import FAQPage from "./pages/OfficialPage/FAQPage";
+// import NoticePage from "./pages/OfficialPage/NoticePage";
+// import RequestPage from "./pages/OfficialPage/RequestPage";
+
+function Layout() {
+  return (
+    <div className="layout">
+      <Navbar />
+      <main className="main">
+        <Outlet />
+      </main>
+      <Footer />
+    </div>
+  );
+}
 
 function App() {
-    const [hello, setHello] = useState('')
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          {/* <Route index element={<LandingPage />} /> */}
 
-    useEffect(() => {
-        axios.get('/api/hello')
-            .then(response => setHello(response.data))
-            .catch(error => console.log(error))
-    }, []);
+          {/* 공통 */}
+          {/* <Route path="/searchCrop" element={<SearchCropPage />} />
+          <Route path="/searchCrop/:cropId" element={<CropDetailPage />} />
+          <Route path="/community" element={<CommunityPage />} />
+          <Route
+            path="/questionCommunity"
+            element={<QuestionCommunityPage />}
+          /> */}
+          {/* <Route path="/faq" element={<FAQPage />} />
+          <Route path="/notice" element={<NoticePage />} />
+          <Route path="/request" element={<RequestPage />} /> */}
 
-    return (
-        <div>
-            백엔드에서 가져온 데이터입니다 : {hello}
-        </div>
-    );
+          {/* 로그인한 사람은 갈 수 없는 경로 */}
+          {/* <Route path="/login" element={<LoginPage />} /> */}
+
+          {/* 로그인한 사람만 갈 수 있는 경로 */}
+          {/* <Route path="/myPage" element={<MyPage />} />
+          <Route path="/myCrop" element={<MyCropPage />} />
+          <Route path="/myCrop/:cropId" element={<MyCropDetailPage />} /> */}
+        </Route>
+      </Routes>
+    </>
+  );
 }
 
 export default App;
