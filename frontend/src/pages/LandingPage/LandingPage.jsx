f1482ff8a40c0e65d3f268c16d652d0ea10a5866
heemin728 <idkhm0728@naver.com>
2024-05-13 01:30:35 +0900

    fix : 오류 수정

--- frontend/src/pages/LandingPage/LandingPage.jsx
+++ frontend/src/pages/LandingPage/LandingPage.jsx
@@ -2,14 +2,6 @@ import React, { useEffect } from "react";
 import axios from "axios";
 
 const LandingPage = () => {
-  useEffect(() => {
-    fetch("http://localhost:8080/login/oauth2/code/google")
-      .then((response) => {
-        const value = response.headers;
-        console.log(value);
-      })
-      .catch((error) => console.log("Error:", error));
-  }, []);
   return <div>LandingPage</div>;
 };
 
