afaff840d55998e47fc9350596a0afb481523c22
Ahnjaemin1208 <michael1208@naver.com>
2024-07-15 20:45:47 +0900

    Update CropController.java

--- backend/src/main/java/MIN/DosiNongBu/controller/crop/CropController.java
+++ backend/src/main/java/MIN/DosiNongBu/controller/crop/CropController.java
@@ -6,6 +6,7 @@ import MIN.DosiNongBu.controller.crop.dto.response.CropListResponseDto;
 import MIN.DosiNongBu.controller.crop.dto.response.CropMainResponseDto;
 import MIN.DosiNongBu.service.crop.CropService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Pageable;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("api/v1")
@@ -36,41 +38,42 @@ public class CropController {
     // 작물 목록 조회
     @GetMapping("/crops")
     public List<CropListResponseDto> cropList(Pageable pageable){
-
+        log.info("작물 목록 조회");
         return cropService.viewCropList(pageable);
     }
 
     // 작물 추천 목록 조회
     @GetMapping("/crops/recommend")
     public List<CropListResponseDto> cropListToRecommend(Pageable pageable){
-
+        log.info("작물 추천 목록 조회");
         return cropService.viewCropRecommendList(pageable);
     }
 
     // 작물 검색 (LIKE)
     @GetMapping("/crops/{keyword}")
     public List<CropListResponseDto> cropListToSearch(@PathVariable String keyword, Pageable pageable){
-
+        log.info("작물 검색(LIKE)");
         return cropService.viewCropRecommendList(pageable);
     }
 
     // 작물 메인 페이지, 기본정보
     @GetMapping("/crops/{cropId}/main")
     public CropMainResponseDto cropMainPage(@PathVariable Long cropId){
-
+        log.info("작물 메인 페이지, 기본 정보");
         return cropService.viewCropMain(cropId);
     }
 
     // 작물 상세 정보 및 관리법
     @GetMapping("crops/{cropId}/info")
     public CropInfoResponseDto cropInfo(@PathVariable Long cropId){
-
+        log.info("작물 상세 정보 및 관리법");
         return cropService.viewCropInfo(cropId);
     }
 
     // 작물 키우기
     @PostMapping("/crops/{cropId}/grow")
     public Long cropGrow(@CookieValue(name = "User") String cookie, @PathVariable Long cropId, @RequestBody UserCropSaveRequestDto requestDto){
+        log.info("작물 키우기");
         Long userId = UserCookie(cookie);
 
         return cropService.registerUserCrop(userId, requestDto);
